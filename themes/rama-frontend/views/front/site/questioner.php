f47c1d40805766a613e0d7bafc65643dbbc9d8ab
wiratama <electro_9@ymail.com>
2015-03-29 12:57:17 +0800

    parsing

--- themes/rama-frontend/views/front/site/questioner.php
+++ themes/rama-frontend/views/front/site/questioner.php
@@ -9,11 +9,12 @@
                 <!-- the quest -->
                 <?php
                 foreach ($model as $questionkey => $question) {
-                    if ($questionkey%2==0) {
-                        echo '<div class="row quest-bgon">';
-                    } else {
-                        echo '<div class="row quest-bgoff">';
-                    }
+                    if ($question->type=='radio') {
+                        if ($questionkey%2==0) {
+                            echo '<div class="row quest-bgon">';
+                        } else {
+                            echo '<div class="row quest-bgoff">';
+                        }
                 ?>
                         <div class="col-md-12">
                         <p><?php echo $question->question; ?></p>
@@ -28,6 +29,25 @@
                         </div>
                     </div>
                 <?php                   
+                    } else if ($question->type=='checkbox') {
+                    if ($questionkey%2==0) {
+                        echo '<div class="col-md-12 bg-on">';
+                    } else {
+                        echo '<div class="col-md-12 bg-off">';
+                    }
+                ?>
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-6 col-md-7 desc-chose"><?php echo $question->question; ?></div>
+                            <?php foreach ($question->answer as $answerkey => $answer) { ?>
+                            <div class="col-xs-12 col-sm-12 col-md-12">
+                                <input type="checkbox" name="questioner[<?php echo $question->id_question;?>][]" value="<?php echo $answer->id_answer;?>">
+                                <?php echo $answer->answer;?>
+                            </div>
+                            <?php } ?>
+                        </div>
+                    </div>
+                <?php
+                    }
                 }
                 ?>
                 <!-- the quest -->        
